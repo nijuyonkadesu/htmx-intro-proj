00888b51a51311471065bdc8f6006e2588133e67
nijuyonkadesu <24ksriramv@gmail.com>
2024-03-16 19:33:12 +0530

    feat(contacts): form to add name and mail

--- cmd/main.go
+++ cmd/main.go
@@ -16,9 +16,35 @@ type Count struct {
 	Count int
 }
 
+type Contact struct {
+	Name  string
+	Email string
+}
+
 // Takes the data and renders the html, with context
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newContact(name, email string) Contact {
+    return Contact{
+        Name:  name,
+        Email: email,
+    }
+}
+
+type Contacts = []Contact
+type Data struct {
+    Contacts Contacts
+}
+
+func newData() Data {
+    return Data{
+        Contacts: Contacts{
+            newContact("John Doe", "jj@mail.com"),
+            newContact("Kiko", "kk@gmail.com"),
+        },
+    }
 }
 
 // Parse out the html
@@ -32,16 +58,26 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
+    data := newData()
+
 	count := Count{Count: 0}
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", data)
 	})
 
 	e.POST("/count", func(c echo.Context) error {
 		count.Count++
 		return c.Render(200, "count", count)
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+        name := c.FormValue("name")
+        email := c.FormValue("email")
+
+        data.Contacts = append(data.Contacts, newContact(name, email))
+		return c.Render(200, "index", data)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
