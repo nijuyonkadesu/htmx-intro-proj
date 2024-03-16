9028dfaa8dddf344a39492dfe6dadc61b40038f8
nijuyonkadesu <24ksriramv@gmail.com>
2024-03-16 16:51:17 +0530

    fix: nexted count button with target

--- cmd/main.go
+++ cmd/main.go
@@ -9,35 +9,40 @@ import (
 )
 
 type Templates struct {
-    templates *template.Template
+	templates *template.Template
 }
 
 type Count struct {
-    Count int 
+	Count int
 }
 
-// Takes the data and renders the html, with context 
-func (t* Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-    return t.templates.ExecuteTemplate(w, name, data)
+// Takes the data and renders the html, with context
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 // Parse out the html
 func newTemplate() *Templates {
-    return &Templates{
-        templates: template.Must(template.ParseGlob("views/*.html")),
-    }
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 }
 
 func main() {
-    e := echo.New()
-    e.Use(middleware.Logger())
-    
-    count := Count{Count: 0}
-    e.Renderer = newTemplate()
-    e.GET("/", func(c echo.Context) error {
-        count.Count++
-        return c.Render(200, "index", count)
-    })
+	e := echo.New()
+	e.Use(middleware.Logger())
 
-    e.Logger.Fatal(e.Start(":42069"))
+	count := Count{Count: 0}
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", count)
+	})
+
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "count", count)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
 }
